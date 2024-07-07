ee3b21a9ae1bf5d889422816dc23aadf4a031825
ivanfabriano <ivanfabrianos@gmail.com>
2024-07-07 11:21:11 +0700

    Show cetak pelanggan in petugas role

--- resources/views/layouts/sections/menu/verticalMenu.blade.php
+++ resources/views/layouts/sections/menu/verticalMenu.blade.php
@@ -53,7 +53,8 @@
                     }
                 @endphp
 
-                @if (auth()->user()->role == 'Petugas' && ($menu->name == 'Form Penggunaan' || $menu->name == 'List Penggunaan'))
+                @if (auth()->user()->role == 'Petugas' &&
+                        ($menu->name == 'Form Penggunaan' || $menu->name == 'List Penggunaan' || $menu->name == 'Cetak Pelanggan'))
                     <li class="menu-item {{ $activeClass }}">
                         <a href="{{ isset($menu->url) ? url($menu->url) : 'javascript:void(0);' }}"
                             class="{{ isset($menu->submenu) ? 'menu-link menu-toggle' : 'menu-link' }}"
@@ -73,7 +74,9 @@
                             @include('layouts.sections.menu.submenu', ['menu' => $menu->submenu])
                         @endisset
                     </li>
-                @elseif (auth()->user()->role == 'Admin' && $menu->name != 'Form Penggunaan' && $menu->name != 'List Penggunaan')
+                @elseif (
+                    (auth()->user()->role == 'Admin' && $menu->name != 'Form Penggunaan' && $menu->name != 'List Penggunaan') ||
+                        $menu->name == 'Cetak Pelanggan')
                     <li class="menu-item {{ $activeClass }}">
                         <a href="{{ isset($menu->url) ? url($menu->url) : 'javascript:void(0);' }}"
                             class="{{ isset($menu->submenu) ? 'menu-link menu-toggle' : 'menu-link' }}"
